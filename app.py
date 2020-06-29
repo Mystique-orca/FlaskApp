5d0ae142224ad9a3ca39bfe4f637372ccf889f1a
Mystique-orca <sumeshnasik@gmail.com>
2020-06-29 02:51:39 -0400

    modified:   app.py 	modified:   static/CSS/main.css 	new file:   static/CSS/navbar.css 	new file:   static/CSS/signin.css 	modified:   templates/index.html 	new file:   templates/login.html 	new file:   templates/signup.html 	modified:   templates/update.html

--- app.py
+++ app.py
@@ -1,11 +1,26 @@
 
-from flask import Flask, render_template, request, url_for, redirect
-from module import create_post, get_posts
+from flask import Flask, flash, render_template, request, url_for, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
+from flask_bootstrap import Bootstrap
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField
+from wtforms.validators import InputRequired, Email, Length
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
+
+
+
 
 #this will initialize the app
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'Mystique'
+Bootstrap(app)
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+
 #database config
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 db = SQLAlchemy(app)
@@ -13,26 +28,45 @@ db = SQLAlchemy(app)
 
 class Posts(db.Model):
 	id = db.Column(db.Integer, primary_key = True)
-	name = db.Column(db.String(20), nullable = False)
-	content = db.Column(db.String(200), nullable = False)
+	title = db.Column(db.String(20), nullable = False)
+	content = db.Column(db.Text, nullable = False)
 	date_created = db.Column(db.DateTime, default=datetime.utcnow)
 
-	def __repr__(self):
-		return '<Posts %r>' %self.id
+class User(UserMixin, db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	username = db.Column(db.String(30), nullable = False)
+	email = db.Column(db.String(30), nullable = False)
+	password = db.Column(db.String(80), nullable = False)
+	date_created = db.Column(db.DateTime, default=datetime.utcnow)
+
+@login_manager.user_loader
+def load_user(user_id):
+	return User.query.get(int(user_id))
 
 
+class LoginForm(FlaskForm):
+	username = StringField('username', validators=[InputRequired(), Length(min=4, max=30)])
+	password = PasswordField('password', validators=[InputRequired(), Length(min=8, max=80)])
+	remember = BooleanField('remember me')
+
+class RegisterForm(FlaskForm):
+	username = StringField('username', validators=[InputRequired(), Length(min=4, max=30)])
+	email = StringField('email', validators=[InputRequired(), Email(message="Invalid Email"), Length(max=30)])
+	password = PasswordField('password', validators=[InputRequired(), Length(min=8, max=80)])
+	
 
 #endpoint specification
 @app.route('/', methods=['GET','POST'])
+@login_required
 def index():
 	
 	if request.method == 'GET':
 		pass
 
 	if request.method == 'POST':
-		name = request.form['name']
+		title = request.form['title']
 		post = request.form['post']
-		new_post = Posts(name = name, content = post)
+		new_post = Posts(title = title, content = post)
 	
 		try:
 			db.session.add(new_post)
@@ -43,11 +77,41 @@ def index():
 
 	else:
 		posts = Posts.query.order_by(Posts.date_created).all()
-		return render_template('index.html', posts=posts)
+		return render_template('index.html', posts=posts, name = current_user.username)
+
+#endpoint specification
+@app.route('/login', methods=['GET','POST'])
+def login():
+	form = LoginForm()
+	if form.validate_on_submit():
+		user = User.query.filter_by(username = form.username.data).first()
+		if user:
+			if check_password_hash(user.password, form.password.data):
+				login_user(user, remember=form.remember.data)
+				return redirect(url_for('index'))
+			else:
+				flash('Invalid Password')
+		else:
+			flash('Invalid Username')
+	return render_template('login.html', form=form)
+
+#endpoint specification
+@app.route('/signup', methods=['GET','POST'])
+def signup():
+	form = RegisterForm()
+	if form.validate_on_submit():
+		hashed_password = generate_password_hash(form.password.data, method='sha256')
+		new_user = User(username = form.username.data, email = form.email.data, password = hashed_password)
+		db.session.add(new_user)
+		db.session.commit()
+		flash('You are good to go!')
+		return redirect(url_for('index'))
+	return render_template('signup.html', form=form)
 
 
 #endpoint specification
 @app.route('/delete/<int:id>', methods=['GET','POST'])
+@login_required
 def delete(id):
 	post_to_delete = Posts.query.get_or_404(id)
 	
@@ -60,6 +124,7 @@ def delete(id):
 
 #endpoint specification
 @app.route('/update/<int:id>', methods=['GET','POST'])
+@login_required
 def update(id):
 	post_to_update = Posts.query.get_or_404(id)
 	
@@ -72,6 +137,13 @@ def update(id):
 			return 'Failed to Update'	
 	else:
 		return render_template('update.html', post=post_to_update)
+
+#endpoint specification
+@app.route('/logout')
+def logout():
+	logout_user()
+	return redirect(url_for('login'))
+
 
 if __name__ == '__main__':
 	app.run(debug=True)
